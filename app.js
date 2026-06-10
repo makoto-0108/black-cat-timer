96f606be235f1ce64fbd7251e4da93910e7a2f98
まこと <makoto.dote0108.86@gmail.com>
2026-06-10 09:42:18 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ document.getElementById("startButton").addEventListener("click", function() {
   document.getElementById("message").textContent = "タイマー開始だよ";
 
   setTimeout(function() {
-    document.getElementById("message").textContent = "そろそろ休憩しない？";
+    document.getElementById("message").textContent = "そろそろ休憩せーへん？";
   }, 2000);
 
 });
