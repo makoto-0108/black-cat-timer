b4be506a9e7238cb016993b88a1bcd65131575d8
まこと <makoto.dote0108.86@gmail.com>
2026-06-10 09:31:11 +0900

    Add timer message after start button click

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 document.getElementById("startButton").addEventListener("click", function() {
+
   document.getElementById("message").textContent = "タイマー開始だよ";
+
+  setTimeout(function() {
+    document.getElementById("message").textContent = "そろそろ休憩しない？";
+  }, 10000);
+
 });
