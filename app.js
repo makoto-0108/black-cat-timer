615eb0f6bb4d54de27f661dfed72c76da5dc1d25
まこと <makoto.dote0108.86@gmail.com>
2026-06-13 10:11:26 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-  const startButton = document.getElementById("startButton");
-  const timerDisplay = document.getElementById("timer");
+  alert("JS読めてる");
 
-  let timerInterval;
+  const startButton = document.getElementById("startButton");
 
   startButton.addEventListener("click", function () {
-
-    clearInterval(timerInterval);
-
-    let timeLeft = 60 * 60;
-
-    timerInterval = setInterval(function () {
-
-      let minutes = Math.floor(timeLeft / 60);
-      let seconds = timeLeft % 60;
-
-      timerDisplay.textContent =
-        String(minutes).padStart(2, "0") +
-        ":" +
-        String(seconds).padStart(2, "0");
-
-      timeLeft--;
-
-      if (timeLeft < 0) {
-        clearInterval(timerInterval);
-      }
-
-    }, 1000);
-
+    alert("スタート押された");
   });
 
 });
