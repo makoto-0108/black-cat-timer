fc6b4b348d5ac824deb1b80497cfb9d7b7fc4f41
まこと <makoto.dote0108.86@gmail.com>
2026-06-18 09:37:45 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
-alert(Notification.permission);
-
 let timerInterval;
-
-// 通知許可
-if ("Notification" in window) {
-  Notification.requestPermission();
-}
-
-// 通知関数
-function sendNotification(title, message) {
-
-  if (Notification.permission === "granted") {
-
-    new Notification(title, {
-      body: message,
-      icon: "cat.png"
-    });
-
-  }
-
-}
 
 // スタート
 document.getElementById("startButton").addEventListener("click", function () {
@@ -35,7 +14,7 @@ document.getElementById("startButton").addEventListener("click", function () {
     document.getElementById("catSpeech");
 
   catSpeech.textContent =
-    "スマホぼちぼちにね";
+    "タイマー開始にゃ";
 
   timerInterval = setInterval(function () {
 
@@ -50,18 +29,13 @@ document.getElementById("startButton").addEventListener("click", function () {
       ":" +
       String(seconds).padStart(2, "0");
 
-    // テスト（開始から約10秒後）
+    // 10秒後テスト
     if (timeLeft === 3590) {
 
-      alert("通知テスト");
+      alert("10秒経過したにゃ");
 
       catSpeech.textContent =
-        "ちょっと休憩するにゃ";
-
-      sendNotification(
-        "🐈‍⬛ 黒猫タイマー",
-        "休憩せーへん？"
-      );
+        "休憩せーへん？";
 
     }
 
@@ -69,7 +43,7 @@ document.getElementById("startButton").addEventListener("click", function () {
     if (timeLeft === 30 * 60) {
 
       catSpeech.textContent =
-        "休憩せーへん？";
+        "ちょっと休憩するにゃ";
 
     }
 
@@ -78,11 +52,6 @@ document.getElementById("startButton").addEventListener("click", function () {
 
       catSpeech.textContent =
         "今日はここまでにしとこか";
-
-      sendNotification(
-        "🐈‍⬛ 黒猫タイマー",
-        "おつかれさま🐈‍⬛"
-      );
 
       clearInterval(timerInterval);
 
