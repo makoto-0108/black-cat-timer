223c215bcf7dddf6e2afbc1c83e99d374ec47585
まこと <makoto.dote0108.86@gmail.com>
2026-06-20 09:57:20 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,58 @@
+const CACHE_NAME = "kuroneko-timer-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./cat.png",
+  "./favicon.png",
+  "./manifest.json"
+];
+
+// インストール時にキャッシュ
 self.addEventListener("install", (event) => {
-  console.log("🐈‍⬛ Service Worker installed");
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+
+        console.log("🐈‍⬛ キャッシュ保存");
+
+        return cache.addAll(urlsToCache);
+
+      })
+
+  );
+
 });
 
+// 有効化
 self.addEventListener("activate", (event) => {
-  console.log("🐈‍⬛ Service Worker activated");
+
+  console.log("🐈‍⬛ Service Worker有効");
+
 });
 
+// オフライン時はキャッシュを返す
 self.addEventListener("fetch", (event) => {
-  // 今は何もしない
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then((response) => {
+
+        // キャッシュがあれば使う
+        if (response) {
+          return response;
+        }
+
+        // なければネットから取得
+        return fetch(event.request);
+
+      })
+
+  );
+
 });
